ef388e7145dd6fcb90901beee84d235be553617b
Ahmad Zedany <zedany7@hotmail.com>
2016-03-11 20:32:36 +0200

    ggg

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "x-token");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 // uncomment after placing your favicon in /public
